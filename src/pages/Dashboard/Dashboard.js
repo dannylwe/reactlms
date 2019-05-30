0dab8521217e88eb549826d5b1c2341325df9b78
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-05-30 17:28:54 +0300

    ft(user dashboard): view parcels - A user should see the articles he has created [Maintains #166352343]

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,39 +1,56 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./Dashboard.css";
 
 class Dashboard extends Component {
   state = {
     items: ""
   };
   componentDidMount() {
-    console.log("user dashboard");
     axios
       .get("https://challenge3andela.herokuapp.com/api/v1/parcels", {
         withCredentials: true
       })
       .then(res => {
         this.setState({ items: res.data["item info"] });
-        console.log(res.data["item info"]);
       });
   }
+  
   render() {
     const { items } = this.state;
-    const itemList = items.map(item => {
-      <>
-        <div>{item.destination}</div>
-        <div>{item.nickname}</div>
-      </>;
-    });
+
+    const itemValues =
+      items.length > 0
+        ? items.map(item => (
+            <tr>
+              <th>{item.destination}</th>
+              <th>{item.nickname}</th>
+              <th>{item.status}</th>
+              <th>{item.pickup}</th>
+            </tr>
+          ))
+        : "loading......";
     return (
       <>
         <nav class="navbar navbar-dark bg-dark">
           <a class="navbar-brand" href="/dashboard">
             Home
           </a>
+          <a class="navbar-brand" href="/login">
+            Logout
+          </a>
         </nav>
-        <h1>user dashboard</h1>
-        <p>under construction</p>
-        {itemList}
+
+        <br />
+        <table>
+          <tr>
+            <th>Destination</th>
+            <th>Order Nickname</th>
+            <th>Status</th>
+            <th>Pickup Location</th>
+          </tr>
+          {itemValues}
+        </table>
       </>
     );
   }
