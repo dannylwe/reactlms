02c35a16d4195392b59c112179504970da113346
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-05-30 15:36:43 +0300

    initial commit

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Dashboard extends Component {
+  state = {
+    items: ""
+  };
+  componentDidMount() {
+    console.log("user dashboard");
+    axios
+      .get("https://challenge3andela.herokuapp.com/api/v1/parcels", {
+        withCredentials: true
+      })
+      .then(res => {
+        this.setState({ items: res.data["item info"] });
+        console.log(res.data["item info"]);
+      });
+  }
   render() {
+    const { items } = this.state;
+    const itemList = items.map(item => {
+      <>
+        <div>{item.destination}</div>
+        <div>{item.nickname}</div>
+      </>;
+    });
     return (
       <>
+        <nav class="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="/dashboard">
+            Home
+          </a>
+        </nav>
         <h1>user dashboard</h1>
         <p>under construction</p>
+        {itemList}
       </>
     );
   }
